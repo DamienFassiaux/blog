4cec463253392bf70e53c74685adbf72cdb23714
Damien Fassiaux <damienfassiaux@gmail.com>
2021-03-15 17:22:11 +0100

    inscription + connexion + liens nav

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class SecurityController extends AbstractController
+{
+
+   
+    /**
+ * @Route("/inscription", name="security_registration")
+ */
+      public function registration(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder): Response
+    {
+        $user = new User(); // on précise à quelle entité va être relié notre formulaire
+
+        $formRegistration = $this->createForm(registrationType::class, $user); // on appel la classe qui permet de construire le formulaire
+
+        dump($request);
+
+        $formRegistration->handleRequest($request);
+
+        dump($user);
+
+        if($formRegistration->isSubmitted() && $formRegistration->isValid())
+                {
+                   $hash = $encoder->encodePassword($user, $user->getPassword()); 
+    
+                    $user->setPassword($hash); 
+
+                    dump($hash);
+       
+                  $manager->persist($user); 
+                  $manager->flush(); // on lance la requete d'insertion
+
+                  $this->addFlash('success', "Félicitations !! Votre compte a bien été validé ! Vous pouvez dés maintenant vous connecter !");
+
+                  return $this->redirectToRoute('security_login');
+
+                  }
+
+    return $this->render('security/registration.html.twig', [
+       'formRegistration' => $formRegistration->createView()
+            ]);
+       
+    }
+
+    /**
+        * @Route("/connexion" , name="security_login")
+        */
+        
+
+    public function login(): Response
+    {
+      return $this->render('security/login.html.twig');
+    }
+
+    /**
+     * @Route("/deconnexion", name="security_logout")
+     */
+
+     public function logout()
+     {
+       //cette méthode ne retourne rien, il suffit d'avoir une route pour se deconnecter
+     }
+
+}
--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Comment;
 use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -22,8 +23,43 @@ class ArticleFixtures extends Fixture
 
             $manager->persist($category);
 
+             // créer entre 4 et 6 articles
+                for($j = 1; $j <= mt_rand(4,6); $j++)
+                {
+                $article = new Article(); // on instancie la class Article() qui se trouve dans le dossier App\Entity
+                $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>';
+                // Nous pouvons maintenant faire appel au setteur pour créer des articles
+                $article->setTitle($faker->sentence())
+                ->setContent($content)
+                ->setImage("https://picsum.photos/seed/picsum/600/400")
+                ->setCreatedAt($faker->dateTimeBetween('-
+                6 months')) // on instancie la classe DateTime() pour formater l'heure
+                ->setCategory($category);
+                $manager->persist($article); // permet de faire persister l'article dans le temps
+                //On donne des commentaires à l'article
+                for($k = 1; $k <= mt_rand(4,10); $k++)
 
-       }
+                {
+                    $comment = new Comment();
+                    $content = '<p>' . join($faker->paragraphs(2), '</p><p>') . '</p>';
+                    // $now = new \DateTime();
+                    // $interval = $now->diff($article->getCreatedAt());
+                    // $days = $interval->days;
+                    // $minimum = '-' . $days . ' days'; // moins -100 days
+                    $days = (new \DateTime())->diff($article->getCreatedAt())->days;
+                   
+                    $comment->setAuthor($faker->name)
+                    ->setContent($content)
+                    ->setCreatedAt($faker->dateTimeBetween('-' . $days . ' days'))
+                    ->setArticle($article);
+
+                    $manager->persist($comment);
+       
         
-    }
+                }
+            }
+        }
+
+        $manager->flush();
+  }
 }
